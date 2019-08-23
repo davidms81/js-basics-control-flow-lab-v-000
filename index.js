805bc3ca064e89204f69bf7e85b9981fe2e66fb6
David Schoenholtz <dmschoenholtz@gmail.com>
2019-08-23 01:07:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function switchOnCharmFromTip(tip){
     case 'generous':
       return `Thank you so much.`
       break
-    case 'not as genorous':
+    case 'not as generous':
       return 'Thank you.'
       break
     default:
